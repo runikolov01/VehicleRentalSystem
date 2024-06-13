3df94645fda83e07b60adf723566be3fed772d90
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-13 11:56:52 +0300

    The implementation for CargoVan class is added

--- VehicleRentalSystem/src/CargoVan.java
+++ VehicleRentalSystem/src/CargoVan.java
@@ -1,2 +1,27 @@
-public class CargoVan {
+public class CargoVan extends Vehicle {
+    private final int driverExperience;
+
+    public CargoVan(String brand, String model, double value, int driverExperience) {
+        super(brand, model, value);
+        this.driverExperience = driverExperience;
+    }
+
+    public int getDriverExperience() {
+        return driverExperience;
+    }
+
+    @Override
+    public double getInsuranceRatePerDay() {
+        return getValue() * 0.0003;
+    }
+
+    @Override
+    public double getRentalCost(int days) {
+        return days > 7 ? 40 : 50;
+    }
+
+    @Override
+    public double getInsuranceAddition() {
+        return (driverExperience > 5) ? -(getInsuranceRatePerDay() * 0.15) : 0;
+    }
 }
