70fe75f8c95d560c28aec9d0fab2eae521ac0b0d
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-13 11:55:58 +0300

    The implementation for Motorcycle class is added

--- VehicleRentalSystem/src/Motorcycle.java
+++ VehicleRentalSystem/src/Motorcycle.java
@@ -1,2 +1,27 @@
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+    private final int riderAge;
+
+    public Motorcycle(String brand, String model, double value, int riderAge) {
+        super(brand, model, value);
+        this.riderAge = riderAge;
+    }
+
+    public int getRiderAge() {
+        return riderAge;
+    }
+
+    @Override
+    public double getInsuranceRatePerDay() {
+        return getValue() * 0.0002;
+    }
+
+    @Override
+    public double getRentalCost(int days) {
+        return days > 7 ? 10 : 15;
+    }
+
+    @Override
+    public double getInsuranceAddition() {
+        return (riderAge < 25) ? (getInsuranceRatePerDay() * 0.2) : 0;
+    }
 }
