e0ac52bb9647c30c5ca8b998e35448160a2c5315
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-13 10:28:19 +0300

    the logic in Vehicle class is added

--- VehicleRentalSystem/src/Vehicle.java
+++ VehicleRentalSystem/src/Vehicle.java
@@ -1,2 +1,31 @@
 public class Vehicle {
+    private String brand;
+    private String model;
+    private double value;
+
+    public Vehicle(String brand, String model, double value) {
+        this.brand = brand;
+        this.model = model;
+        this.value = value;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getInsuranceRate() {
+        return 0;
+    }
+
+    public double getRentalCost(int days) {
+        return 0;
+    }
 }
