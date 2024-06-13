cdcc846e1dd77a2ceb247987dca4d1ab5ec84669
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-13 11:54:23 +0300

    The logic for Car class is added

--- VehicleRentalSystem/src/Car.java
+++ VehicleRentalSystem/src/Car.java
@@ -1,2 +1,27 @@
-public class Car {
+public class Car extends Vehicle {
+    private final int safetyRating;
+
+    public Car(String brand, String model, double value, int safetyRating) {
+        super(brand, model, value);
+        this.safetyRating = safetyRating;
+    }
+
+    public int getSafetyRating() {
+        return safetyRating;
+    }
+
+    @Override
+    public double getInsuranceRatePerDay() {
+        return getValue() * 0.0001;
+    }
+
+    @Override
+    public double getRentalCost(int days) {
+        return days > 7 ? 15 : 20;
+    }
+
+    @Override
+    public double getInsuranceAddition() {
+        return (safetyRating == 4 || safetyRating == 5) ? -(getInsuranceRatePerDay() * 0.1) : 0;
+    }
 }
